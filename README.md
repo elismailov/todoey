20ac98aa489f14a0f91efdfa5afa90ae57fc8bb0
ismailov <ismailowwork@gmail.com>
2024-12-19 16:50:49 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Installation Instructions
+
+To run the project on your local machine, follow these steps:
+
+### **1. Clone the Repository**
+Clone this repository to your local machine:
+```bash
+git clone https://github.com/elismailov/todoey.git
+cd todoey
+```
+
+### **2. Install CocoaPods Dependencies**
+The project uses CocoaPods for dependency management. To install the required dependencies:
+1.	Navigate to the project directory:
+```bash
+cd todoey
+```
+2.	Run the following command:
+```bash
+pod install
+```
+3.	Open the generated .xcworkspace file:
+```bash
+open todoey.xcworkspace
+```
+
+## *Install CocoaPods*
+First Time CocoaPods Users: If you don’t have CocoaPods installed, you can install it using:
+```bash
+sudo gem install cocoapods
+```
+Make sure to run pod install after cloning the repository to set up the necessary libraries.
+
+# Screenshots
+
+<p align="center">
+  <img src="screenshots/categories.png" alt="categories" style="width: 30%;">
+  &nbsp;
+  <img src="screenshots/items.png" alt="items" style="width: 30%;">
+</p>
---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,119 @@
+import UIKit
+import RealmSwift
+import ChameleonFramework
+
+class CategoryViewController: SwipeTableViewController {
+    
+    let realm = try! Realm()
+    
+    var categories: Results<Category>?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadCategories()
+        
+        tableView.separatorStyle = .none
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        guard let navBar = navigationController?.navigationBar.scrollEdgeAppearance else {fatalError("Navigation controller does not exist.")}
+        navBar.backgroundColor = UIColor(hexString: "1D9BF6")
+        navBar.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.white
+        ]
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
+        if let category = categories?[indexPath.row] {
+            cell.textLabel?.text = category.name
+            guard let categoryColor = UIColor(hexString: category.color) else {fatalError()}
+            cell.backgroundColor = categoryColor
+            cell.textLabel?.textColor = ContrastColorOf(categoryColor, returnFlat: true)
+        }
+        
+        return cell
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! TodoListViewController
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categories?[indexPath.row]
+        }
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func save(category: Category) {
+        do {
+            try realm.write {
+                realm.add(category)
+            }
+        } catch {
+            print("Error saving category \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadCategories() {
+        
+        categories = realm.objects(Category.self)
+        
+        tableView.reloadData()
+    }
+    
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) { action in
+            // what will happen once the user clicks the Add Item button on our UIAlert
+            let newCategory = Category()
+            newCategory.name = textField.text!
+            newCategory.color = UIColor.randomFlat().hexValue()
+            
+            self.save(category: newCategory)
+        }
+        
+        alert.addAction(action)
+        
+        alert.addTextField { field in
+            textField = field
+            field.placeholder = "Add a new category"
+        }
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
